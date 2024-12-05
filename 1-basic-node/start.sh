6ec2a08490004f63947a456ee1781b0917c81c04
Bruno Nascimento <brunonascimentodev@gmail.com>
2024-12-05 13:52:41 -0300

    add folder creation if don't exist

--- 1-basic-node/start.sh
+++ 1-basic-node/start.sh
@@ -1,3 +1,9 @@
 # !/bin/bash
 
+dir="besu"
+
+if [[ ! -e $dir ]]; then
+  mkdir $dir
+fi
+
 docker run -p 8545:8545 --mount type=bind,source=./besu/mainnet,target=/var/lib/besu -p 30303:30303 hyperledger/besu:latest --rpc-http-enabled --data-path=/var/lib/besu
--- 3-ckl-network/start.sh
+++ 3-ckl-network/start.sh
@@ -3,5 +3,11 @@
 shopt -s expand_aliases
 source ~/.bashrc
 
-cd ../Node-1
+dir="Node-1"
+
+if [[ ! -e $dir ]]; then
+  mkdir $dir
+fi
+
+cd Node-1
 besu --data-path=data --genesis-file=../cliqueGenesis.json --network-id 123 --rpc-http-enabled --rpc-http-api=ETH,NET,CLIQUE --host-allowlist="*" --rpc-http-cors-origins="all" --metrics-enabled
